c8707d164f8c3fa7bc3b263802d140b9c67d5fff
Temitayo <temfoden@gmail.com>
2024-06-06 19:39:18 +0100

    fix: node depth calc function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
     
-    if (tree->parent != NULL)
+    while (tree->parent != NULL)
     {
         depth++;
         tree = tree->parent;
