8327bcf342efc0cdfa3a644a1f7b9a2315c7522d
Temitayo <temfoden@gmail.com>
2024-06-06 07:17:26 +0100

    fix binary height calc function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,6 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
     if (tree == NULL)
         return 0;
     
+    if (tree->left == NULL && tree->right == NULL)
+        return 0;
+    
     size_t left_height = binary_tree_height(tree->left);
     size_t right_height = binary_tree_height(tree->right);
 
