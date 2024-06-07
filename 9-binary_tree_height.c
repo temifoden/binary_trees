ad35fa5d5fc027a8d186ba9d223df7646384190b
Temitayo Afolabi <temfoden@gmail.com>
2024-06-06 21:20:04 -0400

    fix: styling issues to comply with betty coding style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+size_t left_height, right_height;
 
-    if (tree == NULL)
-        return 0;
+if (tree == NULL)
+return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return 0;
+if (tree->left == NULL && tree->right == NULL)
+return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
 
-    return (MAX(left_height, right_height)) + 1;
+return ((MAX(left_height, right_height)) + 1);
 }
